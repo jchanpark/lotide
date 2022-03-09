c4f816612679fcf4d0a1e62a1259d18c2d2cc451
jchanpark <junpark4936@gamil.com>
2022-03-08 23:46:54 -0500

    comparing two arrays by comparing actual and expected values

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+const eqArrays = function(arr1, arr2) {
+  let count = 0;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        count++;
+      }
+    }
+  } if (count === arr1.length) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (actual === true) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// TEST CODE
+const eqArrayResult = eqArrays([1, 3, 7], [2, 4, 7]);
+assertArraysEqual(eqArrayResult, false);
