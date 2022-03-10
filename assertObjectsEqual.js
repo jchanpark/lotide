972d691b11ec79f1ff99e5a16afc989435295e7c
jchanpark <junpark4936@gamil.com>
2022-03-10 01:54:28 -0500

    comparing two objects and asserting

---
+++ assertObjectsEqual.js
@@ -0,0 +1,25 @@
+const eqObjects = function(obj1, obj2) {
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    return false;
+  }
+  for (let key in obj1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  console.log(`Example label: ${inspect(actual)}`);
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  return;
+};
+
+assertObjectsEqual(eqObjects({ a: '1', b: 2 }, { b: 2, a: '1' }), true);
