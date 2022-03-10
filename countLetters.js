e58a5811dae19f42fe3ad163b231b6861b7f6f6f
jchanpark <junpark4936@gamil.com>
2022-03-09 22:18:23 -0500

    count letters

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  const results = {};
+  for (let letter of str) {
+    if (letter === " ") {
+      continue;
+    }
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+const result1 = countLetters("lighthouse in the house");
+console.log(result1);
+assertEqual(result1.l, 1);
+assertEqual(result1.i, 2);
