cba7197f059c63a0d7ac7682807f6aabac36157f
jchanpark <junpark4936@gamil.com>
2022-03-14 18:11:41 -0400

    refactored using mocha & chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,20 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
 //TEST CODE
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+
+describe("#middle", () => {
+  it("should return [2, 3] when passed [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  })
+
+  it("should return ['Lighthouse'] when passed ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(middle(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse']);
+  })
+
+  it("should return [] when passed ['7', '8']", () => {
+    assert.deepEqual(middle(['7', '8']), []);
+  })
+
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,17 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+//TEST CODE
+describe("#tail", () => {
+  it("should return [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  })
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("should return ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  })
+
+  it("should return [5] for ['Hello', 5]", () => {
+    assert.deepEqual(tail(['Hello', 5]), [5]);
+  })
+});
